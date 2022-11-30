c3d2f0b859ccb8dd0940f67e131f1b2acf35c127
maria-moroz <mma.moroz.mariia@gmail.com>
2022-11-30 14:48:18 +0000

    create project

---
+++ src/test/java/com/project/Team7RecruitmentAndAdmissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7RecruitmentAndAdmissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
