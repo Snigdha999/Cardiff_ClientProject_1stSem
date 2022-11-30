6d38f2fdeec1b45e77ac1d31eafadc0dbad87124
Snigdha Banani - <bananis@cardiff.ac.uk>
2022-11-30 23:45:22 +0000

    Revert "Merge branch 'feature/list-and-add-statistics' into 'main'"
    This reverts merge request !2

--- src/main/java/com/project/repository/StatisticsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import com.project.model.Statistics;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatisticsRepository extends JpaRepository<Statistics, Integer> {
-}
