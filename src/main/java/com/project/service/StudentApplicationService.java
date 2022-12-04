2dd47431b251b8662c30783d9a2a76dfe746123f
Mariia Moroz <morozm@cardiff.ac.uk>
2022-12-04 14:11:29 +0000

    Revert "Merge branch 'development' of..."
    This reverts commit a1cdddfa6e5fec2996c315f71cabd70b41374278

--- src/main/java/com/project/service/StudentApplicationService.java
+++ src/main/java/com/project/service/StudentApplicationService.java
@@ -5,14 +5,7 @@ import com.project.model.StudentApplication;
 import java.util.List;
 
 public interface StudentApplicationService {
+    List<StudentApplication> getAll();
 
-    /* Getting all the data in the Application table
-     * @return List
-     */
-    public List<StudentApplication> getAll();
-
-    /* Adding all the data in the Application table
-     * @return List
-     */
-    public void add(StudentApplication studentApplication);
+    void add(StudentApplication studentApplication);
 }
