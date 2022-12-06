691f579354115360810d9522aff7b659f485fde6
Snigdha Banani - <bananis@cardiff.ac.uk>
2022-12-06 21:26:09 +0000

    added update functionality
    Signed-off-by: Snigdha Banani - <bananis@cardiff.ac.uk>

--- src/main/java/com/project/controller/StudentApplicationController.java
+++ src/main/java/com/project/controller/StudentApplicationController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,5 +41,13 @@ public class StudentApplicationController {
         return "redirect:/applications";
     }
 
+    @GetMapping("/updateStudentApplication/{id}")
+    public String updateStudentApplication(@PathVariable (value = "id") int id){
+        ModelAndView model = new ModelAndView("edit-application");
+        StudentApplication application = studentApplicationService.getStudentApplicationById(id);
+        model.addObject("updateApplication", application);
+        System.out.println(application);
+        return "updateApplication";
+    }
 
 }
