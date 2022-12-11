d7ef2d8903c0ee79bc5054ed7b0b769b4e2e8267
Snigdha Banani - <bananis@cardiff.ac.uk>
2022-12-11 18:35:32 +0000

    Added access modifier and code comments for StatisticsService class
    Signed-off-by: Snigdha Banani - <bananis@cardiff.ac.uk>

--- src/main/java/com/project/service/StatisticsServiceImpl.java
+++ src/main/java/com/project/service/StatisticsServiceImpl.java
@@ -59,6 +59,9 @@ public class StatisticsServiceImpl implements StatisticsService {
         this.statisticsRepository.deleteById(id);
     }
 
+    /* Find all the data in the Statistics table on the basis of study year
+     * @return List
+     */
     @Override
     public List<Statistics> findAllByStudyYear() {
         //If data is available, the list information is processed in descending
@@ -88,6 +91,8 @@ public class StatisticsServiceImpl implements StatisticsService {
         }
     }
 
+    /* Delete all the data
+     */
     @Override
     public void deleteAll() {
         statisticsRepository.deleteAll();
