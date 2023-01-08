750d0f48fd43cc3d313b9015d010166ba7fa990e
weipeng <PengW2@cardiff.ac.uk>
2023-01-08 21:48:29 +0000

    Add some tests in the StatisticsServiceTest.

--- src/test/java/com/project/StatisticsServiceTest.java
+++ src/test/java/com/project/StatisticsServiceTest.java
@@ -2,12 +2,16 @@ package com.project;
 
 import com.project.model.Statistics;
 import com.project.service.StatisticsService;
+import com.project.repository.StatisticsRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.data.domain.Page;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import java.util.List;
 
 
@@ -23,6 +27,39 @@ public class StatisticsServiceTest {
         List<Statistics> statistics = statisticsService.getAll();
         Assertions.assertThat(statistics.size()).isGreaterThan(0);
     }
+
+
+    @Test
+    public void whenDeleteAllStatistics_thenReturnNoStatistics() {
+        List<Statistics> statistics = statisticsService.getAll();
+        statisticsService.deleteAll();
+        Assertions.assertThat(statistics.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testFindStatisticPaginated(){
+        Page<Statistics> page= statisticsService.findStatisticPaginated(2,5,"ucasCode","asc");
+        Assertions.assertThat(page.getSize()).isEqualTo(5);
+
+    }
+
+    @Test
+    void testFindAllByStudyYear_noStatistics() {
+        // mock the StatisticsRepository to return an empty list
+        StatisticsRepository mockRepo = mock(StatisticsRepository.class);
+        when(mockRepo.findAll()).thenReturn(List.of());
+
+        // mock the StatisticsService to call the real findAllByStudyYear method
+        StatisticsService mockService = mock(StatisticsService.class);
+        when(mockService.findAllByStudyYear()).thenCallRealMethod();
+
+        List<Statistics> result = mockService.findAllByStudyYear();
+
+        // assert that the result list has a single element with 0 places
+        assertEquals(1, result.size());
+        assertEquals(0, result.get(0).getPlaces());
+    }
+
 
     @Test
     public void addTest() {
