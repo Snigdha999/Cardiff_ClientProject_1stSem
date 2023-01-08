a17f687a23803958293c9874fb54c81bd815b0bf
maria-moroz <mma.moroz.mariia@gmail.com>
2023-01-08 22:01:48 +0000

    Adding tests

--- src/test/java/com/project/StudentApplicationServiceTest.java
+++ src/test/java/com/project/StudentApplicationServiceTest.java
@@ -1,5 +1,6 @@
 package com.project;
 
+import com.project.model.ApplicationStatus;
 import com.project.model.StudentApplication;
 import com.project.service.StudentApplicationService;
 import org.assertj.core.api.Assertions;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -18,14 +20,6 @@ public class StudentApplicationServiceTest {
 
     @Autowired
     private StudentApplicationService studentApplicationService;
-
-
-
-    @Test
-    public void whenGetAllStudentApplication_thenReturnAllStudentApplication() {
-        List<StudentApplication> studentApplication = studentApplicationService.getAll("09456712");
-        Assertions.assertThat(studentApplication.size()).isGreaterThan(0);
-    }
 
     @Test
     public void whenDeleteAllStudentApplication_thenReturnNoStudentApplication() {
@@ -56,6 +50,43 @@ public class StudentApplicationServiceTest {
     public void testGetAmountApplications(){
        int response= studentApplicationService.getAmountApplications();
         Assertions.assertThat(response).isEqualTo(0);
+    }
+
+    @Test
+    public void whenGetAllStudentApplication_thenReturnAllStudentApplication() {
+        StudentApplication application = new StudentApplication();
+        application.setStudentNumber("09456712");
+        studentApplicationService.add(application);
+        List<StudentApplication> studentApplications = studentApplicationService.getAll("09456712");
+        Assertions.assertThat(studentApplications.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void whenParseExcelApplicationStatus_thenArrayOfApplicationStatusesReturned(){
+        // creating new application object
+        StudentApplication application = new StudentApplication();
+        // crating array list for an expected application status with values CF and UF
+        ArrayList<ApplicationStatus> expected = new ArrayList<>();
+        expected.add(ApplicationStatus.CF);
+        expected.add(ApplicationStatus.UF);
+        List<ApplicationStatus> response = application.parseExcelApplicationStatus("CF,UF");
+        // comparing response with an expected value
+        Assertions.assertThat(response).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDisplayApplicationStatusCalled_thenFormattedStringWithStatusValuesReturned(){
+        // creating new application object
+        StudentApplication application = new StudentApplication();
+        // setting application statuses CF and UF
+        ArrayList<ApplicationStatus> applicationStatuses = new ArrayList<>();
+        applicationStatuses.add(ApplicationStatus.CF);
+        applicationStatuses.add(ApplicationStatus.UF);
+        application.setApplicationStatus(applicationStatuses);
+        // calling displayApplicationStatus method
+        String response = application.displayApplicationStatus();
+        // comparing response with an expected value
+        Assertions.assertThat(response).isEqualTo("CF,UF");
     }
 
 }
