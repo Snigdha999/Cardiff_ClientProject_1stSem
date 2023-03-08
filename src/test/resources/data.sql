a51eef3e2c682ab5345d4e608e0e2fbd7ef30db9
Snigdha Banani - <bananis@cardiff.ac.uk>
2023-03-08 11:48:11 +0000

    h2 database
    Signed-off-by: Snigdha Banani - <bananis@cardiff.ac.uk>

---
+++ src/test/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO `accounts`
+(id,account_type, password, username, is_super_user)
+VALUES
+    ('B720','UFBWMIDA','23/11/2017','2022/3','21129186');
+
+INSERT INTO `applications`
+(id,application_status, application_status_comments, confirmation_comments, contextual, correspondence_lang, country, )
+VALUES
+    ('B720','UFBWMIDA','23/11/2017','2022/3','21129186');
+
+INSERT INTO `statistics`
+(id,offers, places, study_year)
+VALUES
+    ('B720','UFBWMIDA','23/11/2017','2022/3','21129186');
+
